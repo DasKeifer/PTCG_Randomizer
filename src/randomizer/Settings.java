ddb5d23c69aa4a1bb82d9a003ec4a8611c091379
Das Keifer <EplanumProductions@gmail.com>
2020-08-28 23:23:29 -0400

    Minor setting changes after investigation of if discard types can be changed

--- src/randomizer/Settings.java
+++ src/randomizer/Settings.java
@@ -1,32 +1,44 @@
 package randomizer;
 
 import java.util.Map;
-import java.util.HashMap;
-
 import constants.CardDataConstants.CardType;
 
 public class Settings 
 {
+	// TODO: Generation is very stretch - don't worry too much about for now
     public enum RandomizationStrategy 
     {
-        UNCHANGED, SHUFFLE, RANDOM,
+        UNCHANGED, SHUFFLE, RANDOM, GENERATED
     }
 
     public class TypeSpecificData 
     {
-    	// default is 0% for 0 moves, 50% for 1 move, and 50% for 2 moves
-    	int[] percentWithNumMoves = new int[] {0, 50, 50};
+    	// Only if RANDOM or GENERATED
+    	// TODO get from cards - only applicable for Random or generated
+    	int[] percentWithNumMoves = new int[] {0, 50, 50}; // Need to do logic to make sure it matches with two following ones
+    	int[] percentWithNumAttacks = new int[] {0, 50, 30};
+    	int[] percentWithNumPowers = new int[] {0, 20, 0};
     }
-
-    // Poke Move Settings
-	private boolean movesWithinType;
-	private boolean movesMmatchPokeSpecific;
-	private boolean movesMatchTypeSpecific;
-	private boolean movesForceOneDamaging;
-	private boolean movesForceOneNonPokePower;
-	private Map<CardType, TypeSpecificData> movesTypeSpecificData;
-
-	private boolean movesPokePowerWithMoves;
+    
+    // Poke Move specific settings 
 	private RandomizationStrategy movesStrat;
+	private Map<CardType, TypeSpecificData> movesTypeSpecificData;
+	
+    // applicable if not UNCHANGED
+	private boolean movesKeepSameNumberOfAttacks;
+	private boolean movesForceOneDamaging; // If keep same number of attacks is off
+	private boolean movesAttacksWithinType; // Effects if GENERATED
+	
+	
+	// Poke power specific (applicable if not UNCHANGED)
 	private RandomizationStrategy movesPokePowerStrat;
+	
+	// applicable if not UNCHANGED
+	private boolean movesKeepSameNumberOfPowers;
+	private boolean movesPowersWithinType; // Effects if GENERATED
+
+	// Think these can be removed - but may be simpler to keep for now
+    // Move/Power Applicable (both use the same) (applicable if not UNCHANGED)
+	// private boolean movesMatchPokeSpecific;
+	// private boolean movesMatchTypeSpecific;
 }
